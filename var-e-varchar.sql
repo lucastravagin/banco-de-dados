9a93ee748262929dd037fefb689880974c405a3f
Lucas Travagin <lucas_travagin1112@hotmail.com>
2020-04-29 18:13:58 -0300

    Criando a modelagem física das tabelas

---
+++ var-e-varchar.sql
@@ -0,0 +1,9 @@
+/* Comparando Var e Varchar
+CADA CARACTER ARMAZENADO EM UM CAMPO NO BANCO DE DADOS EQUIVALE A 1 BYTE, ou seja um conjunto de 8 bits
+Se eu armazenar a palavra LUCAS no banco de dados, estarei utilizando 5 Bytes, ou 40 Bits
+
+É importante saber utilizar o tipo de um campo no banco de dados, pois
+
+CHAR - VALOR FIXO - O NÚMERO NUNCA VAI VARIAR
+VARCHAR - VALOR VARIÁVEL
+*/
