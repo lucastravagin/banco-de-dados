4be75ded7b611ddffc5c4a0e08047f317f2992a9
Lucas Travagin <lucas_travagin1112@hotmail.com>
2020-04-29 17:49:36 -0300

    first commit

---
+++ modelage-logica-e-fisica.sql
@@ -0,0 +1,38 @@
+/* Para que ser um banco de dados
+
+Um banco de dados armazena informações e dados de uma aplicação
+O banco de dados relacional é composto por tabelas, essas tabelas possuem linha e colunas. A linha é chamada
+de tupla. Ele gerencia os dados
+Arquitetura de Software
+Padrão MVC
+Model - Representado por tabelas do banco de dados
+View - Representa a parte do client
+Controller - Reponsável pela Regra de Negócio da aplicação
+Modelagem Lógica e Modelagem Física
+Modelagem Lógica: É um documento de requisitos
+*/
+
+/*
+Modelagem Básica
+
+Cliente
+
+NOME - CARACTER(30)
+CPF - NUMERICO(11)
+EMAIL - CARACTER(100)
+TELEFONE - CARACTER(30)
+ENDERECO - CARACTER(100)
+SEXO CARACTER(1)
+*/
+
+/*
+Processos de Modelagem
+AD
+Modelagem Conceitual - Rascunho
+Modelagem Lógica - Qualquer programa de Modelagem
+
+DBA - AD
+Modelagem Física - Scripts do banco
+*/
+
+
