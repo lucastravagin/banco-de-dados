127ebebe9e44a8a4a966027bbe9c1ba8e9aa3893
Lucas Travagin <lucas_travagin1112@hotmail.com>
2020-04-29 18:20:27 -0300

    Conhecendo o Enum e Numericos

---
+++ enum-e-numericos.sql
@@ -0,0 +1,7 @@
+/* Enum e Numéricos
+
+Enum - Vem de enumeração, ou seja um conjunto de dados enumerados, utilizamos quando queremos fixar os dados em um coluna
+Ele funciona como uma ComboBox no sistema, os dados que entrarão seráo pré definidos
+
+Numéricos: Temos o tipo int, para numeros inteiros e float para numeros reais. O Float vem de ponto flutuante. 
+*/
