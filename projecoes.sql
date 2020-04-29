89d9fa93a0e9a2d982ea0894b857aa9dcd403978
Lucas Travagin <lucas_travagin1112@hotmail.com>
2020-04-29 20:53:53 -0300

    Trabalhando com projeções

---
+++ projecoes.sql
@@ -0,0 +1,14 @@
+/**Conhecendo as projeções/
+
+/*Projeção: É oque aparece na tela: Select
+-Projetando os dados na tela que o usuário pediu
+*/
+
+/*Mostrar Dados e Hora*/
+
+SELECT NOW() as DATA_HORA;
+
+SELECT 'LUCAS TRAVAGIN' as nome;
+
+
+
