39377d038f756c531f79e15ad4c7b19f005005e3
Lucas Travagin <lucas_travagin1112@hotmail.com>
2020-05-20 19:46:08 -0300

    Operadores lógicos

---
+++ operadores_logicos.sql
@@ -0,0 +1,11 @@
+/* Operadores lógicos
+OR --> Para que a saida da query seja verdadeira, basta que apenas uma condição seja verdadeira
+AND --> Para que a saida da query seja verdadeira, todas as condições devem ser verdaediras
+*/
+
+SELECT * FROM livros;
+
+
+SELECT * FROM livros where NOME_LIVRO = 'LIVRO DE JAVA'
+
+
